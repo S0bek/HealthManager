b89fdb6a8de58caabf562344107a5d0b319478b7
S0bek <dav_fed@outlook.com>
2015-03-15 22:25:40 +0100

    Application désormais fonctionnelle, en cours d'optimisation du design

--- app/src/main/java/net/hackdevicz/s0bek/healthmanager/MainActivity.java
+++ app/src/main/java/net/hackdevicz/s0bek/healthmanager/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 //mapping contexte-vue
 import net.hackdevicz.s0bek.healthmanager.classes.User;
@@ -36,40 +37,21 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //récupération des données de chaque élément de la vue, les variables sont quant à elles déclarées au dessus pour les avoir en global
-        //dans toute l'Activité courante
         size = (EditText) findViewById(R.id.size_value);
         size.setPadding(10,5,5,5);
-        //String size_content = size.getText().toString();
 
         weight = (EditText) findViewById(R.id.weight_value);
         weight.setPadding(10,5,5,5);
-        //String weight_content = weight.getText().toString();
 
         age = (EditText) findViewById(R.id.age_value);
         age.setPadding(10,5,5,5);
-        //String age_content = age.getText().toString();
 
         IMC = (TextView) findViewById(R.id.imc_value);
-        user_IMC = IMC.getText().toString();
-
         IMG = (TextView) findViewById(R.id.img_value);
-        user_IMG = IMG.getText().toString();
-
-        String size_content = size.getText().toString();
-        String weight_content = weight.getText().toString();
-        String age_content = age.getText().toString();
 
         //on récupère les données une fois que toutes les données ont bien été saisies
-        if (!size_content.isEmpty() && !weight_content.isEmpty() && !age_content.isEmpty() && !sexe.isEmpty()) {
-
-            user_size = Double.parseDouble(size_content);
-            user_weight = Double.parseDouble(weight_content);
-            user_age = Integer.parseInt(age_content);
-
-            Button result = (Button) findViewById(R.id.Result);
-            result.setOnClickListener(this);
-        }
+        Button result = (Button) findViewById(R.id.Result);
+        result.setOnClickListener(this);
 
     }
 
@@ -101,7 +83,24 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         int id = v.getId();
         if (id == R.id.Result) {//clic sur le bouton de calcul
 
-            //récupération des données de la vue
+            //récupération des données de la vue une fois seulement que l'utilisateur a cliqué sur le bouton de calcul
+
+            user_IMC = IMC.getText().toString();
+            user_IMG = IMG.getText().toString();
+
+            String size_content = size.getText().toString();
+            String weight_content = weight.getText().toString();
+            String age_content = age.getText().toString();
+
+            //on récupère les données uniquement si elles sont remplies, sinon ca plante!
+            if (!size_content.isEmpty() && !weight_content.isEmpty() && !age_content.isEmpty() && !sexe.isEmpty()) {
+
+                user_size = Double.parseDouble(size_content);
+                user_weight = Double.parseDouble(weight_content);
+                user_age = Integer.parseInt(age_content);
+
+            }
+
             user.setSize(user_size);
             user.setAge(user_age);
             user.setSexe(sexe);
@@ -117,11 +116,11 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             Log.e("SEXE:", "" + user.getSexe());
             Log.e("WEIGHT:", "" + user.getWeight());
 
-            //IMC.setText(user_IMC + " " + imc_result);
-            //IMG.setText(user_IMG + " " + img_result);
-
-
-            //Toast.makeText(this, "Calcul effectué", Toast.LENGTH_SHORT).show();
+            IMC.setText(user_IMC + " " + imc_result);
+            IMG.setText(user_IMG + " " + img_result);
+            Toast.makeText(this, "Calcul effectue", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Votre IMC: " + imc_result, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Votre IMG: " + img_result, Toast.LENGTH_SHORT).show();
         }
 
     }
